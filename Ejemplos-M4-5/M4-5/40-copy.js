db604f26442f97baebefccd83410d3b15b5214f9
josepereza <48152618+josepereza@users.noreply.github.com>
2019-08-03 10:35:36 +0200

    Add files via upload

---
+++ Ejemplos-M4-5/M4-5/40-copy.js
@@ -0,0 +1,25 @@
+// nombre del programa:  40-copy.js
+
+// copy con callback -  Ej: node 40-copy.js origen.txt destino.txt
+
+var fs = require('fs'); // Imports file system module
+
+if (process.argv.length != 4) { // Wrong parameters?
+  console.log('   syntax: "node copy <orig> <dest>"');
+  process.exit() // Finalizes node process
+}
+
+fs.readFile(
+  process.argv[2], // <orig> file
+  function (err, data) { // callback when read finishes
+    if (err) throw err;
+    fs.writeFile(
+      process.argv[3], // <dest> file
+      data, // <orig> data to be written  
+      function (err) { // callback when write finishes
+        if (err) throw err;
+        console.log('   file copied');
+      }
+    );
+  }
+);
